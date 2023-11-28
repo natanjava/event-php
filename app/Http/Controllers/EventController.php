625c96c36b8e312194a02d351d068e746f08de04
Natan Bonfim <natan.nbg@gmail.com>
2023-11-28 09:50:12 -0300

    avoiding repetition in Event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -62,9 +62,25 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if($user) {
+
+            $userEvents = $user->eventAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent) {
+                if($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
+        
     }
 
     public function dashboard() {
